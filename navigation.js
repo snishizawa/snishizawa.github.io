542d97d62e04b38ec6aa8ea351d4e81f8cfea7f9
snishizawa <sin.nishizawa@gmail.com>
2024-11-15 10:35:51 +0900

    Add files via upload

---
+++ navigation.js
@@ -0,0 +1,41 @@
+﻿function navi(){
+
+	var html="";
+	html += '<div id="masthead">';
+	html += '<div id="masthead_text">';
+	html += '<a href="../index.html">DAシンポジウム 2016</a>';
+	html += '</div>';
+	html += '</div>';
+	
+	document.write(html);
+	
+	document.write("data");
+		
+
+	$.ajax({
+		url: "navi.html",
+		datatype: "html",
+		async: false,
+/*		success: function(data){
+			document.write(data);*/
+	    success: function(xhr, status, err) {
+		   alert('HTML読み出しで問題がありました:' + url);
+
+		}
+	    error: function(xhr, status, err) {
+   		   alert('HTML読み出しで問題がありました:' + url);
+    	}
+	});
+/*	
+
+	var html="";
+	html += '<div id="masthead">';
+	html += '<div id="masthead_text">';
+	html += '<a href="../index.html">DAシンポジウム 2016</a>';
+	html += '</div>';
+	html += '</div>';
+	
+	document.write(html);
+
+*/	
+}
